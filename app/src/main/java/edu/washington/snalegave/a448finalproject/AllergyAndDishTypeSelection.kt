f08bdb5da3fde277f11221c79a0f357f765a2733
Sohrab <sohrabseera@hotmail.com>
2018-05-30 15:44:27 -0700

    Adding allergy and dish type selection xml

---
+++ app/src/main/java/edu/washington/snalegave/a448finalproject/AllergyAndDishTypeSelection.kt
@@ -0,0 +1,12 @@
+package edu.washington.snalegave.a448finalproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AllergyAndDishTypeSelection : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_allergy_and_dish_type_selection)
+    }
+}
